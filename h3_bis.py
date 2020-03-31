4dfba80be213d9cf978a352b20f42a299263467c
Andrei Damian <andrei@lummetry.ai>
2020-03-31 09:49:22 +0300

    added 1st pre-draft of literature review

--- h3_bis.py
+++ h3_bis.py
@@ -11,8 +11,10 @@ import torch as th
 from datetime import datetime as dt
 import json
 import scipy
-from torch_utils import InputPlaceholder, L2_Normalizer, TripletLoss, get_activation
+from torch_utils import InputPlaceholder, L2_Normalizer, TripletLoss, get_activation, ModelTrainer
 import nli
+
+from sklearn.metrics import classification_report
 
 lst_log = []
 _date = dt.now().strftime("%Y%m%d_%H%M")
@@ -41,6 +43,11 @@ def l_glv(w):
 def arr(u,v):
   return np.array((u,v)).astype(np.float32)
 
+def dist(w1,w2):
+  w1_emb = GLOVE[w1.lower()]
+  w3_emb = GLOVE[w2.lower()]
+  return round(scipy.spatial.distance.cosine(w1_emb, w3_emb),3)
+  
 
 def _get_triplets_candidates(data, embeds, thr=0.2):
   pos_examples = [x for x in data if x[1] == 1]
@@ -59,7 +66,7 @@ def _get_triplets_candidates(data, embeds, thr=0.2):
     Pr("Processed {:.1f}%".format((i+1)/len(pos_examples)*100))
   return candidates
 
-def get_train_dev(trn, dev):
+def get_train_dev(trn, dev, return_triplets=False):
   train_trip = _get_triplets_candidates(data=trn, embeds=GLOVE)
   lst_x_train = [[l_glv(t[0]), l_glv(t[1]), l_glv(t[2])] for t in train_trip]
   x_train = np.array(lst_x_train).astype(np.float32)
@@ -69,7 +76,10 @@ def get_train_dev(trn, dev):
       vector_func=l_glv, 
       vector_combo_func=arr,
       )  
-  return x_train, x_dev, y_dev
+  if return_triplets:
+    return x_train, x_dev, y_dev, train_trip
+  else:
+    return x_train, x_dev, y_dev
 
 
 class ThSiamTrainer(th.nn.Module):
@@ -80,7 +90,7 @@ class ThSiamTrainer(th.nn.Module):
                bn_inputs=False,
                bn=False,
                activ='relu',
-               drop=0.3,
+               drop=0.5,
                siam_norm=True,):
     super().__init__()
     siam_layers = [InputPlaceholder(input_dim)]
@@ -121,143 +131,32 @@ class ThSiamTrainer(th.nn.Module):
     
 
 
-class TripletBasedClassifier():
+class TripletBasedClassifier(ModelTrainer):
   def __init__(self,
-               lr=0.001,
-               max_patience=10,
-               max_fails=30,
-               lr_decay=0.5,
-               device=th.device("cuda" if th.cuda.is_available() else "cpu"),
-               model_name='H3TB'
+               beta=0.2,
+               thr=0.5,
+               input_dim=None,
+               model_name='H3TB',
+               **kwargs,
                ):
-    self.batch_size=32
-    self.device = device
-    self.lr = lr
-    self.model_name = model_name
-    self.max_patience = max_patience
-    self.max_fails = max_fails
-    self.model = None
-    self.lr_decay = lr_decay
-    self.no_remove=False
-    self.errors= []
+    super().__init__(model_name=model_name,**kwargs)
+    self.thr = thr
+    self.beta = beta
+    self.input_dim = input_dim
     return
   
-  def fit(self, X, y):
-    # does nothing
-    return
+
+  def define_graph(self):
+    if self.input_dim is None:
+      raise ValueError("Unknown model input dimension")
+    self.model = ThSiamTrainer(input_dim=self.input_dim)
+    
+    
+  def define_loss(self):
+    self.loss = TripletLoss(beta=self.beta, device=self.device)
   
-  def fit_triplets(self, x_train_triplets, x_dev, y_dev, epochs=1000, batch_size=32, dev_data=None):
-    self.epochs = epochs
-    self.batch_size = batch_size
-    n_obs = x_train_triplets.shape[0]
-    embed_size = x_train_triplets.shape[-1]
-    if self.model is None:
-      self.model = ThSiamTrainer(input_dim=embed_size).to(self.device)
-      
-    th_dl = th.utils.data.DataLoader(
-        th.utils.data.TensorDataset(th.tensor(x_train_triplets, dtype=th.float32, device=self.device)),
-        batch_size=self.batch_size
-        )
-    self.model.train()
-    loss = TripletLoss(device=self.device, beta=0.2)
-    optimizer = th.optim.Adam(self.model.parameters(), lr=self.lr)
-    patience = 0
-    fails = 0
-    best_f1 = 0
-    best_fn = ''
-    not_del_fns = []
-    for epoch in range(1, self.epochs + 1):
-      epoch_losses = []
-      for batch_iter, (X_batch,) in enumerate(th_dl):
-        batch_embeds = self.model(X_batch)
-        th_loss = loss(batch_embeds)
-        optimizer.zero_grad()
-        th_loss.backward()
-        optimizer.step()
-        epoch_losses.append(th_loss.detach().cpu().numpy())
-        Pr("Training epoch {} - {:.1f}% - avg loss: {:.3f},  Patience {}/{},  Fails {}/{}\t\t\t\t\t".format(
-            epoch, 
-            (batch_iter + 1) / (n_obs // self.batch_size + 1) * 100,
-            np.mean(epoch_losses),
-            patience, self.max_patience,
-            fails, self.max_fails))
-      # end epoch
-      self.errors.append(np.mean(epoch_losses))
-      if dev_data is not None:
-        predictions = self.predict_on_data(dev_data)
-      else:
-        predictions = self.predict(x_dev)
-      macrof1 = utils.safe_macro_f1(y_dev, predictions)         
-         
-      self.model.train()
-      if macrof1 > best_f1:
-        patience = 0
-        fails = 0
-        last_best_fn = best_fn
-        best_fn = "models/{}_e{:03}_F{:.4f}.th".format(
-            self.model_name, epoch, macrof1)
-        best_epoch = epoch
-        P("\rFound new best macro-f1 {:.4f} > {:.4f} at epoch {}. \t\t\t".format(macrof1, best_f1, epoch))
-        best_f1 = macrof1
-        th.save(self.model.state_dict(), best_fn)
-        th.save(optimizer.state_dict(), best_fn + '.optim')
-        if last_best_fn != '':
-          try:
-            os.remove(last_best_fn)
-            os.remove(last_best_fn + '.optim')
-          except:
-            not_del_fns.append(last_best_fn)
-            not_del_fns.append(last_best_fn + '.optim')
-      else:
-        patience += 1
-        fails += 1
-        Pr("Finished epoch {}. Current score {:.3f} < {:.3f}. Patience {}/{},  Fails {}/{}".format(
-            epoch, macrof1, best_f1, patience, self.max_patience, fails, self.max_fails))
-        if patience > self.max_patience:
-          lr_old = optimizer.param_groups[0]['lr'] 
-          lr_new = lr_old * self.lr_decay
-          self.model.load_state_dict(th.load(best_fn))
-          optimizer.load_state_dict(th.load(best_fn + '.optim'))
-          for param_group in optimizer.param_groups:
-            param_group['lr'] = lr_new
-          P("\nPatience reached {}/{}  -  reloaded from ep {} reduced lr from {:.1e} to {:.1e}".format(
-              patience, self.max_patience, best_epoch, lr_old, lr_new))
-          patience = 0
-          
-        if fails > self.max_fails:
-          P("\nMax fails {}/{} reached!".format(fails, self.max_fails))
-          break
-          
-      
-    # end all epochs
-    if best_fn != '':
-      P("Loading model from epoch {} with macro-f1 {:.4f}".format(best_epoch, best_f1))
-      self.model.load_state_dict(th.load(best_fn))        
-      not_del_fns.append(best_fn + '.optim')
-      if (best_f1 < 0.67) and (not self.no_remove):
-        P("  Removing '{}'".format(best_fn))        
-        not_del_fns.append(best_fn)
-      else:
-        os.rename(best_fn, "models/{}.th".format(self.model_name))
-    P("  Cleaning after fit...")
-    atmps = 0
-    while atmps < 10 and len(not_del_fns) > 0:   
-      removed = []
-      for fn in not_del_fns:
-        if os.path.isfile(fn):
-          try:
-            os.remove(fn)
-            removed.append(fn)
-            P("  Removed '{}'".format(fn))
-          except:
-            pass
-        else:
-          removed.append(fn)            
-      not_del_fns = [x for x in not_del_fns if x not in removed]
-      
-    return
   
-  def predict(self, X):
+  def predict(self, X, return_dist=False):
     self.model.eval()
     with th.no_grad():
       th_X = th.tensor(X, device=self.device, dtype=th.float32)
@@ -265,30 +164,33 @@ class TripletBasedClassifier():
       th_w2 = th_X[:,1]
       th_w1_e = self.model.get_embeds(th_w1)
       th_w2_e = self.model.get_embeds(th_w2)
-      th_dist = (th_w1_e - th_w2_e).abs().sum(1)
-      th_preds = th_dist > 0.5
+      th_dist = th.sqrt(th.pow(th_w1_e - th_w2_e, 2)).mean(1)
+      th_preds = th_dist < self.thr
+      dists = th_dist.cpu().numpy()
       preds = th_preds.cpu().numpy()    
     self.model.train()
-    return preds
+    if return_dist:
+      return preds, dists
+    else:
+      return preds
 
-  def predict_on_data(self, data):
-    X, y = nli.word_entail_featurize(
+  
+  def evaluate(self, data=None, verbose=False):
+    if data is None and self.validation_data is None:
+      raise ValueError('Validation data not defined!')
+    data = self.validation_data if data is None else data
+    X_dev, y_dev = nli.word_entail_featurize(
         data=data, 
         vector_func=l_glv, 
         vector_combo_func=arr,
         )  
-    self.model.eval()
-    with th.no_grad():
-      th_X = th.tensor(X, device=self.device, dtype=th.float32)
-      th_w1 = th_X[:,0]
-      th_w2 = th_X[:,1]
-      th_w1_e = self.model.get_embeds(th_w1)
-      th_w2_e = self.model.get_embeds(th_w2)
-      th_dist = (th_w1_e - th_w2_e).abs().sum(1)
-      th_preds = th_dist > 0.5
-      preds = th_preds.cpu().numpy()    
-    self.model.train()
-    return preds
+    preds, dists = self.predict(X_dev, return_dist=True)
+    self._debug_data = [[data[i][0][0], data[i][0][1], data[i][1], dists[i]] for i in range(18)]
+    macrof1 = utils.safe_macro_f1(y_dev, preds)
+    if verbose:
+      self.P(classification_report(y_dev, preds))
+    return macrof1
+
   
 
 if __name__ == '__main__':
@@ -302,26 +204,31 @@ if __name__ == '__main__':
       NLIDATA_HOME, 'nli_wordentail_bakeoff_data.json')  
   
   utils.fix_random_seeds()
-  GLOVE_DIM = 50
+  GLOVE_DIM = 300
     
   if "GLOVE" not in globals() or len(next(iter(GLOVE.values()))) != GLOVE_DIM:
     P("Loading GloVe-{}...".format(GLOVE_DIM))
     GLOVE = utils.glove2dict(os.path.join(GLOVE_HOME, 'glove.6B.{}d.txt'.format(GLOVE_DIM)))  
     P("GloVe-{} loaded.".format(GLOVE_DIM))  
     
-  with open(wordentail_filename) as f:
-    wordentail_data = json.load(f)      
-    
-  train_data = wordentail_data['word_disjoint']['train']
-  dev_data = wordentail_data['word_disjoint']['dev']
   
   if 'x_trn' not in globals():
-    x_trn, x_dev, y_dev = get_train_dev(train_data, dev_data)
+    with open(wordentail_filename) as f:
+      wordentail_data = json.load(f)      
+      
+    train_data = wordentail_data['word_disjoint']['train']
+    dev_data = wordentail_data['word_disjoint']['dev']
+    x_trn, x_dev, y_dev, lst_trip = get_train_dev(train_data, dev_data, return_triplets=True)
   
-  clf = TripletBasedClassifier(lr=0.0001)
-  clf.fit_triplets(
-      x_train_triplets=x_trn,
-      x_dev=x_dev,
-      y_dev=y_dev,
-      dev_data=dev_data,
+  clf = TripletBasedClassifier(
+      beta=1,
+      lr=0.0001,
+      thr=0.037,
+      input_dim=GLOVE_DIM,
+      validation_data=dev_data,
       )
+  clf.fit(
+      x_train=x_trn,
+      )
+
+  clf.evaluate(dev_data, verbose=True)
--- torch_utils.py
+++ torch_utils.py
@@ -4,8 +4,13 @@ Created on Sat Mar 28 10:21:23 2020
 
 @author: Andrei
 """
-
+import numpy as np
 import torch as th
+import os
+import textwrap
+
+def Pr(s=''):
+  print('\r' + str(s), end='', flush=True)
 
 
 class InputPlaceholder(th.nn.Module):
@@ -52,9 +57,219 @@ class TripletLoss(th.nn.Module):
   
   def __repr__(self):
     s = self.__class__.__name__ + "(beta={})".format(
-        self.margin,
+        self.beta,
         )
     return s  
+  
+
+class ModelTrainer():
+  def __init__(self,
+               opt=th.optim.Adam,
+               lr=0.001,
+               max_patience=10,
+               max_fails=30,
+               cooldown=2,
+               lr_decay=0.5,
+               batch_size=32,
+               score_mode='max',
+               device=th.device("cuda" if th.cuda.is_available() else "cpu"),
+               model_name='',
+               validation_data=None,
+               base_folder='models',
+               ):
+    self.score_mode = score_mode
+    self.opt = opt
+    self.batch_size=batch_size
+    self.device = device
+    self.lr = lr    
+    self.model_name = model_name
+    self.max_patience = max_patience
+    self.max_fails = max_fails
+    self.cooldown = cooldown
+    self.model = None
+    self.loss = None
+    self.optimizer = None
+    self.lr_decay = lr_decay
+    self.lr_decay_iters = 0
+    self.no_remove=False
+    self.errors= []
+    self._last_best_fn = ''
+    self._not_del_fns = []
+    self.base_folder = base_folder
+    self.validation_data = validation_data
+    self._debug_data = None
+    if not hasattr(self, 'P'):
+      def _P(s):
+        print(s, flush=True)
+      setattr(self, 'P', _P)
+    return
+  
+  
+  def __repr__(self):
+    _s  = "  Model: '{}'+\n".format(self.model_name)
+    _s += textwrap.indent(str(self.model), " " * 4)
+    _s += textwrap.indent("Loss: {}\n".format(self.loss), " " * 4)
+    return _s
+  
+  def define_graph(self):
+    raise ValueError("You must subclass ModelTrainer and overwrite `define_graph`")
+
+  def define_loss(self):
+    raise ValueError("You must subclass ModelTrainer and overwrite `define_loss`")
+    
+  def evaluate(self):
+    raise ValueError("You must subclass ModelTrainer and overwrite `evaluate`")
+    
+  def predict(self):
+    raise ValueError("You must subclass ModelTrainer and overwrite `predict`")
+  
+  def train_on_batch(self, batch):    
+    if len(batch) == 2:
+      th_x, th_y = batch
+    elif len(batch) == 1:
+      th_x = batch[0]
+      th_y = None
+    th_yh = self.model(th_x)
+    if th_y is not None:
+      th_loss = self.loss(th_yh, th_y)
+    else:
+      th_loss = self.loss(th_yh)
+    self.optimizer.zero_grad()
+    th_loss.backward()
+    self.optimizer.step()
+    err = th_loss.detach().cpu().numpy()
+    return err
+    
+  
+  
+  def fit(self, x_train, y_train=None, epochs=10000):
+    if self.model is None:
+      self.define_graph()    
+    if self.loss is None:
+      self.define_loss()
+    if self.optimizer is None:
+      self.optimizer = self.opt(self.model.parameters(), lr=self.lr)
+    self.model.to(self.device)
+    tensors = [th.tensor(x, device=self.device) for x in [x_train,y_train] if x is not None]
+    th_ds = th.utils.data.TensorDataset(*tensors)
+    th_dl = th.utils.data.DataLoader(
+        th_ds, 
+        batch_size=self.batch_size,
+        shuffle=True)
+    n_obs = len(th_dl)
+    self.P("\nTraining model:\n  {}\n  Training on {} observations.\n".format(
+      self,
+      tensors[0].shape[0]))
+    patience = 0
+    fails = 0
+    best_epoch = -1
+    best_score = -np.inf if self.score_mode == 'max' else np.inf
+    eval_func = max if self.score_mode == 'max' else min
+    for epoch in range(1, epochs + 1):
+      epoch_errors = []
+      for batch_iter, batch_data in enumerate(th_dl):
+        err = self.train_on_batch(batch_data)
+        epoch_errors.append(err)
+        Pr("Training epoch {} - {:.1f}% - avg loss: {:.3f},  Patience {}/{},  Fails {}/{}\t\t\t\t\t".format(
+            epoch, 
+            (batch_iter + 1) / (n_obs // self.batch_size + 1) * 100,
+            np.mean(epoch_errors),
+            patience, self.max_patience,
+            fails, self.max_fails,
+            ))
+      # end epoch
+      score = self.evaluate()
+      if eval_func(score, best_score) != best_score:
+        self.P("\rFound new best score {:.4f} better than {:.4f} at epoch {}. \t\t\t".format(score, best_score, epoch))
+        self.save_model(epoch, score)
+        best_score = score
+        best_epoch = epoch
+      else:
+        patience += 1
+        if patience > 0:
+          fails += 1
+        self.P("\rFinished epoch {}, loss: {:.4f}. Current score {:.3f} < {:.3f}. Patience {}/{},  Fails {}/{}".format(
+            epoch, np.mean(epoch_errors), score, best_score, patience, self.max_patience, fails, self.max_fails))
+        if patience >= self.max_patience:
+          self.P("Patience reached {}/{} - reloading from epoch and reducting lr".format(
+              patience, self.max_patience, best_epoch))
+          self.reload_best()
+          self.reduce_lr()
+          patience = -self.cooldown
+        if fails >= self.max_fails:
+          self.P("\nMax fails {}/{} reached!".format(fails, self.max_fails))
+          break     
+    self.restore_best_and_cleanup()
+    return self
+          
+    
+  
+  
+  def save_model(self, epoch, score, cleanup=True, verbose=True):
+    if not os.path.isdir(self.base_folder):
+      os.mkdir(self.base_folder)
+    best_fn = self.base_folder + "/{}_e{:03}_F{:.4f}.th".format(
+            self.model_name, epoch, score)
+    th.save(self.model.state_dict(), best_fn)
+    th.save(self.optimizer.state_dict(), best_fn + '.optim')
+    if verbose:
+      self.P("  Saved: '{}'".format(best_fn))
+      self.P("  Saved: '{}'".format(best_fn + '.optim'))
+    if self._last_best_fn != '':
+      self._not_del_fns.append(self._last_best_fn)
+      self._not_del_fns.append(self._last_best_fn + '.optim')
+    self._last_best_fn = best_fn
+    if cleanup:
+      self.cleanup_files()
+    return    
+  
+    
+  
+  def reload_best(self, verbose=True):
+    self.model.load_state_dict(th.load(self._last_best_fn))
+    self.optimizer.load_state_dict(th.load(self._last_best_fn + '.optim'))
+    if verbose:
+      self.P("  Reloaded model & optimizer '{}'".format(self._last_best_fn))
+    return
+  
+  
+  def reduce_lr(self, verbose=True):
+    self.lr_decay_iters += 1
+    factor = self.lr_decay ** self.lr_decay_iters
+    for i, param_group in enumerate(self.optimizer.param_groups):
+      lr_old = param_group['lr'] 
+      param_group['lr'] = param_group['lr'] * factor
+      lr_new = param_group['lr']
+    if verbose:
+      self.P("  Reduced lr from {:.1e} to {:.1e}".format(lr_old, lr_new))
+    return
+    
+
+
+  def cleanup_files(self):
+    atmps = 0
+    while atmps < 10 and len(self._not_del_fns) > 0:   
+      removed = []
+      for fn in self._not_del_fns:
+        if os.path.isfile(fn):
+          try:
+            os.remove(fn)
+            removed.append(fn)
+            self.P("  Removed '{}'".format(fn))
+          except:
+            pass
+        else:
+          removed.append(fn)            
+      self._not_del_fns = [x for x in self._not_del_fns if x not in removed]
+    return
+  
+  
+  def restore_best_and_cleanup(self):
+    self.reload_best()
+    self._not_del_fns.append(self._last_best_fn + '.optim')
+    self.cleanup_files()
+    return
+    
   
   
 def get_activation(act):
